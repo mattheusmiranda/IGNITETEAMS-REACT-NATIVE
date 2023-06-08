9fa70f92d767776ba2ed5f5125ad2075e8863b2b
Matheus Miranda <106291807+mattheusmiranda@users.noreply.github.com>
2023-06-07 23:40:37 -0300

    Criado boão para a criação de turmas.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,20 +1,48 @@
 import { Container } from './styles';
+import { useState } from 'react';
+
+import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
+
 import { Highlight } from '@components/Highlight';
-import { GroupCard } from "@components/GroupCard"
+import { GroupCard } from '@components/GroupCard';
+
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
+
 
 export function Groups() {
+    const [groups, setGroups] = useState<string[]>(["Turma do truco"]);
+
     return (
         <Container>
 
             <Header />
+
             <Highlight
-                title='Turmas'
-                subTitle='Jogue com a sua turma'
+                title="Turmas"
+                subTitle="Jogue com a sua turma"
             />
 
-            <GroupCard
-                title='Galera do Ignite '
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <GroupCard
+                        title={item}
+                    />
+                )}
+
+                contentContainerStyle={groups.length === 0 && { flex: 0.7 }}
+                ListEmptyComponent={() => (
+                    <ListEmpty
+                        message="Cadastre a primeira turma"
+                    />
+                )}
+            />
+
+            <Button
+                title='Criar nova turma'
             />
 
         </Container>
