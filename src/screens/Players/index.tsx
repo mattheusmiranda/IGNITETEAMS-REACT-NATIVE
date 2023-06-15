5e9e5de55f65ce65baea369f5933c8507f15f959
Matheus Miranda <106291807+mattheusmiranda@users.noreply.github.com>
2023-06-14 23:20:18 -0300

    Criação de componente Filter, inclusão de scroll lateral no componente.

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,10 +1,17 @@
-import { Container, Form } from './styles';
+import { FlatList } from 'react-native';
+import { Container, Form, HeaderList, NuberOfPlayers } from './styles';
+import { Filter } from '@components/Filter';
 import { Input } from '@components/Input';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { ButtonIcon } from '@components/ButtonIcon';
+import { useState } from 'react';
 
 export function Players() {
+
+    const [team, setTeam] = useState('Time A');
+    const [player, setPlayer] = useState([team]);
+
     return (
         <Container>
             <Header showBackButton />
@@ -22,8 +29,28 @@ export function Players() {
 
                 <ButtonIcon
                     icon={'add'}
+                    type='PRIMARY'
                 />
             </Form>
+
+            <HeaderList>
+                <FlatList
+                    data={['Time A', 'Time B', 'Time C', 'Time D', 'Time E', 'Time F', 'Time G']}
+                    keyExtractor={item => item}
+                    renderItem={({ item }) => (
+                        <Filter
+                            title={item}
+                            isActive={item === team}
+                            onPress={() => setTeam(item)}
+                        />
+                    )}
+                    horizontal
+                />
+
+                <NuberOfPlayers>
+                    {player.length}
+                </NuberOfPlayers>
+            </HeaderList>
 
         </Container>
     );
--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
@@ -14,4 +14,19 @@ export const Form = styled.View`
     flex-direction: row;
     justify-content: center;
     border-radius: 6px;
+`;
+
+export const HeaderList = styled.View`
+    width: 100%;
+    flex-direction: row;
+    align-items: center;
+    margin: 32px 0px 12px;
+`;
+
+export const NuberOfPlayers = styled.Text`
+    ${({ theme }) => css`
+        color: ${theme.COLORS.GRAY_200};
+        font-family: ${theme.FONT_FAMILY.BOLD};
+        font-size: ${theme.FONT_SIZE.SM}px;
+    `};
 `;
